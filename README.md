1cee31e0606e229ef748a8c4ac79157fc5cf29e9
Liz Lee <aihsliz31@gmail.com>
2024-09-10 11:30:55 +0900

    special method / magic method : double underscore

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 
 ## Chapter 2. Data Model
 
-### 20240906
+### 20240910
+```ch02_02.py 파일 참고```
+- special method, magic method
+- 파이썬의 중요한 핵심 프레임워크 : 시퀀스 (Sequence), 반복 (Iterator), 함수 (Functions), 클래스 (Class)
+- underscore __ 2개씩 있는 것 
+e.g.) '__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', 
+'__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__index__', '__init__', '__init_subclass__', '__int__', '__invert__', '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__', 'as_integer_ratio', 'bit_count', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 'numerator', 'real', 'to_bytes'
+
+
+### 20240909
 ```ch02_01.py 파일 참고```
 - 데이터 모델 (Data Model)
 - 참조 : https://docs.python.org/3/reference/datamodel.html
---
+++ ch02_02.py
@@ -0,0 +1,57 @@
+# 매직메소드 실습 
+# 파이썬의 중요한 핵심 프레임워크 : 시퀀스 (Sequence), 반복 (Iterator), 함수 (Functions), 클래스 (Class)
+
+print(int)
+
+# 모든 속성 및 메소드 출력
+print(dir(int))
+
+n =100
+
+print(n + 200)
+print(n.__add__(200))
+print(n.__doc__)
+print(n.__bool__(), bool(n)) # 0은 false, 0 이외의 값은 true
+print(n * 100, n.__mul__(100))
+
+
+print()
+print()
+
+class Student:
+    def __init__(self, name, height):
+        self._name = name
+        self._height = height
+
+    def __str__(self):
+        return 'Student Class Info : {}, {}'.format(self._name, self._height)
+
+    def __ge__(self, x): # greater or equal to
+        print('Called. >> __ge__ Method.')
+        if self._height >= x._height:
+            return True
+        else:
+            return False
+
+    def __le__(self, x): # less than or equal to
+        print('Called. >> __le__ Method.')
+        if self._height <= x._height:
+            return True
+        else:
+            return False
+
+    def __sub__(self, x): # less than or equal to
+        print('Called. >> __sub__ Method.')
+        return self._height - x._height
+
+
+s1 = Student('James', 181)
+s2 = Student('Liz', 167)
+
+# print(s1 + s2)  연산은 지원하지 않음 
+
+# 키를 서로 비교 하고 싶음 
+# magic method / special method 를 override 해서 사용 할 수 있음
+print(s1 >= s2)
+print(s1 <= s2)
+print(s1 - s2)
