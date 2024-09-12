901067983c7cf5d02916bd99c49ff45959330465
Liz Lee <aihsliz31@gmail.com>
2024-09-12 12:12:30 +0900

    tuple, *, packing & unpacking, sorted vs sort

--- ch03_01.py
+++ ch03_01.py
@@ -94,3 +94,72 @@ print(marks2)
 print([id(i) for i in marks1])
 print([id(i) for i in marks2])
 
+
+# Tuple 
+# Packing & Unpacking
+
+a, b = divmod(100, 9)
+print(a,b)
+
+print(divmod(100, 9))
+print(divmod(*(100, 9)))
+
+x, y, *rest = range(10)
+print(x, y, rest)
+
+x, y, *rest = range(2)
+print(x, y, rest)
+
+x, y, *rest = 1, 2, 3, 4, 5
+print(x, y, rest)
+
+
+# mutable vs immutable
+
+l = (10, 15, 20)
+m = [10, 15, 20]
+
+print(l,id(l))
+print(m, id(m))
+
+
+l = l * 2
+m = m * 2
+
+print(l,id(l))
+print(m, id(m))
+
+# l[0] = 2  tuple object does not support item assignment
+
+l *= 2
+m *= 2
+
+print(l,id(l)) # Tuple 은 아이디가 달라짐
+print(m, id(m)) # 리스트는 아이디가 같음 
+
+m[0] = 2
+print(m, id(m))
+
+# sort vs sorted 
+# reverse (역순), key=len (글자 길이에 따른 순서), key=str.lower, key=func
+
+f_list = ['orange', 'apple', 'mango', 'papaya', 'lemon', 'straberry', 'coconut']
+
+# sorted : 정렬 후 '새로운' 객체 반환
+
+print(f_list, id(f_list))
+sorted_list = sorted(f_list)
+reversed_list = sorted(f_list, reverse=True)
+print(reversed_list, id(reversed_list))
+
+print(sorted(f_list, key=len))
+print(sorted(f_list, key=lambda x:x[-1]))
+
+# sort : 정렬 후 객체 직접 변경
+# 반환 값 확인 
+# 반환 값이 None 일 경우 이 함수는 반환값이 없는 메소드라고 알려주는 시그널 --> 이 함수는 객체를 직접 변경하는 함수구나. 
+
+a = f_list.sort() 
+
+print(a, f_list)
+print(id(f_list.sort(reverse=True)), id(f_list))
